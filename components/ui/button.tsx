44caac80c28f3ad64cc1bef370be60a5f7fee1e0
Aurora Scharff <aurora.sofie@gmail.com>
2025-12-19 09:30:11 +0100

    refactor: migrate components from radix-ui to base-ui

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,5 @@
+import { Button as ButtonPrimitive } from '@base-ui/react/button';
 import { cva, type VariantProps } from 'class-variance-authority';
-import { Slot } from 'radix-ui';
-import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
@@ -44,23 +43,9 @@ function Button({
   className,
   variant = 'default',
   size = 'default',
-  asChild = false,
   ...props
-}: React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean;
-  }) {
-  const Comp = asChild ? Slot.Root : 'button';
-
-  return (
-    <Comp
-      data-slot="button"
-      data-variant={variant}
-      data-size={size}
-      className={cn(buttonVariants({ className, size, variant }))}
-      {...props}
-    />
-  );
+}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+  return <ButtonPrimitive data-slot="button" className={cn(buttonVariants({ className, size, variant }))} {...props} />;
 }
 
 export { Button, buttonVariants };
